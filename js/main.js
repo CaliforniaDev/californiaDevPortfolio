8f9baccddc7684b73499e968cb6cdce2389a9abd
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-09-08 06:30:15 -0700

    Overhaul add styling and behavior

--- js/main.js
+++ js/main.js
@@ -7,23 +7,33 @@
     
     function documentIsReady() {
         const hamburger = document.querySelector(".hamburger");
-        const navMenu = document.querySelector("#mobile-nav-menu");
+        const navMenu = document.querySelector("nav");
         const navOverlay = document.querySelector(".nav-overlay");
         const navLink = document.querySelectorAll(".nav-link");
+        const main = document.querySelector("main"); 
+        const nav = document.querySelector("nav");
      
-        
+        nav.addEventListener("mouseenter", openNav);
+        nav.addEventListener("mouseleave", closeNav)
         hamburger.addEventListener("click", openMobileNav);
         navLink.forEach(n => n.addEventListener("click", closeMobileMenu));
-    
+        function openNav() {
+            main.classList.toggle("active-desktop");
+        }
+        function closeNav() {
+            main.classList.toggle("active-desktop");
+        }
         function openMobileNav() {
             hamburger.classList.toggle("active");
             navMenu.classList.toggle("active");
             navOverlay.classList.toggle("active");
+            main.classList.toggle("active");
         }
         function closeMobileMenu() {
             hamburger.classList.toggle("active");
             navMenu.classList.toggle("active");
             navOverlay.classList.toggle("active");
+            main.classList.toggle("active");
         }
     
         const appHeight = () => {
@@ -35,7 +45,7 @@
 
         $('a').click(function(){
             $('html, body').animate({
-                scrollTop: $( $(this).attr('href') ).offset().top-220
+                scrollTop: $( $(this).attr('href') ).offset().top-250
             }, 500);
             return false;
         });
